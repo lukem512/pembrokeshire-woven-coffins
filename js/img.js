dfded4d23e1415be33baa44671d2d9b0357beee4
lukem512 <lukem95@gmail.com>
2025-12-11 10:23:55 +0000

    Auto-rotate carousel images

--- js/img.js
+++ js/img.js
@@ -10,27 +10,37 @@ function swapImage(el, src, alt) {
     }
 }
 
+function updateActiveClass(images, activeIndex) {
+    images.forEach((img, index) => {
+        if (index === activeIndex) {
+            img.classList.add('active');
+        } else {
+            img.classList.remove('active');
+        }
+    });
+}
+
 function carousel(el) {
     if (el && el.classList.contains('carousel')) {
-      const mainEl = el.querySelector('img.main');
-      const images = el.querySelectorAll('img');
+        const mainEl = el.querySelector('img.main');
+        const images = el.querySelectorAll('img');
       
-      // Add click event listener to each image
-      images.forEach((img, index) => {
-          img.addEventListener('click', () => {
-              // Add 'active' class to clicked image
-              img.classList.add('active');
+        // Add click event listener to each image
+        images.forEach((img, index) => {
+            img.addEventListener('click', () => {
+                updateActiveClass(images, index);
+                swapImage(mainEl, img.src, img.alt);
+            });
+        });
 
-              // Remove 'active' class from other images
-              images.forEach((otherImg, otherIndex) => {      
-                  if (otherIndex !== index) {
-                      otherImg.classList.remove('active');
-                  }
-              });
-
-              // Swap the image onto the main display
-              swapImage(mainEl, img.src, img.alt);
-          });
-      });
+        // Rotate images every 5 seconds
+        if (el.classList.contains('auto')) {
+            let currentIndex = 0;
+            setInterval(() => {
+                currentIndex = (currentIndex + 1) % images.length;
+                updateActiveClass(images, currentIndex);
+                swapImage(mainEl, images[currentIndex].src, images[currentIndex].alt);
+            }, 5000);
+        };
     }
 }
